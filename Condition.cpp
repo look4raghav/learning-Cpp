988df3643dd5be30c184a7211cab8fe53fae9b46
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-07-07 09:04:20 +0530

    Create Condition.cpp

---
+++ Condition.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    int x, y;
+    cin>>x>>y;
+    // YOUR CODE GOES HERE
+    if(x<=y)
+    {
+    cout << "Robin" << endl;}
+    else{
+    cout << "Rahul" << endl;}
+    return 0;
+}
