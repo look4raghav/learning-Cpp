14cfdedcf3c457d363c7669e6a118a20ca362711
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-07-19 00:54:35 +0530

    Create upper&lower_bound.cpp

---
+++ upper&lower_bound.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<iterator>
+using namespace std;
+
+int main()  {
+    // YOUR CODE GOES HERE
+    // Please take input and print output to standard input/output (stdin/stdout)
+    // E.g. 'cin' for input & 'cout' for output
+    int n;
+    cin >> n;
+    vector<int>v(n);
+    for(int i=0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+    int q;
+    cin >> q;
+    vector<int>::iterator lower;
+    for(int i=0; i<q; i++)
+    {
+        int m;
+        cin >> m;
+        lower = lower_bound(v.begin(), v.end(), m);
+        cout << lower-v.begin() << endl;
+    }
+    return 0;
+}
