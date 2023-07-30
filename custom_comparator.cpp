476c52661f059e0a358bd946db545156043f2a38
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-07-30 12:33:34 +0530

    Create custom_comparator.cpp

---
+++ custom_comparator.cpp
@@ -0,0 +1,12 @@
+bool comp(pair<int, int> a, pair<int, int> b)
+{
+    if((a.first + a.second) < (b.first + b.second))
+    {
+        return true;
+    }
+    return false;
+}
+
+void sortArray(vector<pair<int, int>> &A){
+    sort(A.begin(), A.end(), comp);
+}
